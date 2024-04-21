f4407c750f707b0b444414b21ec4ebef5321f995
Matt Stiles <mattstiles@gmail.com>
2024-04-21 10:10:22 -0700

    latest

--- fetch_all_products.py
+++ fetch_all_products.py
@@ -9,16 +9,15 @@ from botocore.exceptions import NoCredentialsError, ClientError
 today = pd.Timestamp("today").strftime("%Y_%m_%d")
 
 def create_s3_client(profile_name=None):
-    """ Create an S3 client instance with the specified profile or default environment credentials. """
+    """Create an S3 client instance with the specified profile or default environment credentials."""
     if profile_name:
         session = boto3.Session(profile_name=profile_name)
     else:
         session = boto3.Session()
-    s3_client = session.client('s3')
-    return s3_client
+    return session.client('s3')
 
 def fetch_all_products():
-    """ Fetches all products from the ALDI API with pagination handling. """
+    """Fetches all products from the ALDI API with pagination handling."""
     headers = {
         'accept': 'application/json, text/plain, */*',
         'accept-language': 'en-US,en;q=0.9',
@@ -40,7 +39,6 @@ def fetch_all_products():
         'sort': 'relevance',
         'merchantReference': '479-022'
     }
-
     response = requests.get(base_url, headers=headers, params=params)
     response.raise_for_status()
     data = response.json()
@@ -53,22 +51,20 @@ def fetch_all_products():
         params['page[offset]'] = (page - 1) * 48
         response = requests.get(base_url, headers=headers, params=params)
         page_data = response.json()
-
-        for product in page_data['data'][0]['attributes']['catalogSearchProductOfferResults']:
-            products.append({
-                'sku': product['productConcreteSku'],
-                'name': product['name'],
-                'brand_name': product['brandName'],
-                'price_unit': product['comparisonPriceUnit'],
-                'slug': product['urlSlugText'],
-                'formatted_price': product.get('prices', [{}])[0].get('formattedPrice', 'N/A'),
-                'snap_eligible': product['usaSnapEligible']
-            })
+        products.extend([{
+            'sku': product['productConcreteSku'],
+            'name': product['name'],
+            'brand_name': product['brandName'],
+            'price_unit': product['comparisonPriceUnit'],
+            'slug': product['urlSlugText'],
+            'formatted_price': product.get('prices', [{}])[0].get('formattedPrice', 'N/A'),
+            'snap_eligible': product['usaSnapEligible']
+        } for product in page_data['data'][0]['attributes']['catalogSearchProductOfferResults']])
 
     return pd.DataFrame(products)
 
 def fetch_product_details(sku):
-    """ Fetch detailed product information based on SKU. """
+    """Fetch detailed product information based on SKU."""
     headers = {
         'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
         'Accept': 'application/json, text/plain, */*',
@@ -78,10 +74,7 @@ def fetch_product_details(sku):
         'sec-ch-ua-platform': '"macOS"',
     }
     product_details_url = f'https://api.aldi.us/v1/products/{sku}'
-    product_params = {
-        'servicePoint': '479-022',
-        'serviceType': 'pickup',
-    }
+    product_params = {'servicePoint': '479-022', 'serviceType': 'pickup'}
     
     response = requests.get(product_details_url, headers=headers, params=product_params)
     response.raise_for_status()
@@ -97,42 +90,39 @@ def fetch_product_details(sku):
         'warning_desc': product_data['warnings'][0]['message'] if product_data['warnings'] else None
     }
 
-def upload_file_to_s3(file_name, bucket, object_name, profile_name=None):
-    """ Uploads a file to an S3 bucket using the specified profile or environment credentials. """
-    s3_client = create_s3_client(profile_name)
-    try:
-        s3_client.upload_file(file_name, bucket, object_name)
-    except NoCredentialsError:
-        logging.error("Credentials not available")
-        return False
-    except ClientError as e:
-        logging.error(e)
-        return False
-    return True
-
 def main():
     profile_name = "haekeo" if 'AWS_ACCESS_KEY_ID' not in os.environ else None
     
+    # Directories and file paths
+    local_dir = "data/processed/"
+    os.makedirs(local_dir, exist_ok=True)
+    csv_file = f"{local_dir}aldi_products_detailed_{today}.csv"
+    json_file = f"{local_dir}aldi_products_detailed_{today}.json"
+    
     all_products_df = fetch_all_products()
     detailed_info = []
+    
     for sku in tqdm(all_products_df['sku'], desc="Hydrating products"):
         details = fetch_product_details(sku)
         detailed_info.append(details)
-
+    
     detailed_df = pd.DataFrame(detailed_info)
     full_df = all_products_df.merge(detailed_df, on='sku', how='left')
 
-    csv_file = f'aldi_products_detailed_{today}.csv'
-    json_file = f'aldi_products_detailed_{today}.json'
-
+    # Save locally
     full_df.to_csv(csv_file, index=False)
-    full_df.to_json(json_file, indent=4, orient='records')
+    full_df.to_json(json_file, orient='records', indent=4)
 
     # Upload to S3
-    upload_file_to_s3(csv_file, 'stilesdata.com', 'aldi/aldi_products_detailed_latest.csv', profile_name=profile_name)
-    upload_file_to_s3(json_file, 'stilesdata.com', 'aldi/aldi_products_detailed_latest.json', profile_name=profile_name)
+    s3_client = create_s3_client(profile_name)
+    s3_bucket = 'stilesdata.com'
+    s3_path = 'aldi/'
+    s3_client.upload_file(csv_file, s3_bucket, f"{s3_path}aldi_products_detailed_latest.csv")
+    s3_client.upload_file(json_file, s3_bucket, f"{s3_path}aldi_products_detailed_latest.json")
+    s3_client.upload_file(csv_file, s3_bucket, f"{s3_path}aldi_products_detailed_{today}.csv")
+    s3_client.upload_file(json_file, s3_bucket, f"{s3_path}aldi_products_detailed_{today}.json")
 
-    print("Data saved and uploaded to S3. Process completed.")
+    print("Data saved locally and uploaded to S3. Process completed.")
 
 if __name__ == "__main__":
     main()
